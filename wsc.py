a8f63fe15bbcd4c5aa44024b276a52d96faa1ad5
Benji Steenhoek <benjaminjsteenhoek@gmail.com>
2017-12-01 13:04:45 -0500

    Added fix for service id. Use template_service_id to get current service id, but check if it's None first.

--- wsc.py
+++ wsc.py
@@ -17,36 +17,54 @@ username, password, database = get_login_info()
 con = connect(user=username, password=password, database=database)
 cursor = con.cursor()
 
+template_service_id = None
+
+
 def create_record(data):
     print('Creating record in the database with these records...')
     for key in data:
         print('{} : {},'.format(key, data[key]))
 
+
 @bottle.route('/')
 def hello():
-    # Submit a record to the db
-    if bottle.request.params:
-        # params = list(map(lambda key : bottle.request.params.getall(key), bottle.request.params))
-        create_record(bottle.request.params)
-
     # Get service dates and times from the db
     cursor.execute("""
-    SELECT Svc_DateTime
+    SELECT Service_ID, Svc_DateTime
     FROM Service
     """)
-    svc_datetimes = cursor.fetchall()
-    svc_datetimes_string = '\n'.join(['<option>{}</option>'.format(dt[0].strftime('%m/%d/%y %I:%M %p')) for dt in svc_datetimes])
+    svc_datetimes_with_id = cursor.fetchall()
+    svc_datetimes = [pair[1] for pair in svc_datetimes_with_id]
+    svc_datetimes_string = '\n'.join(
+        ['<option>{}</option>'.format(dt.strftime('%m/%d/%y %I:%M %p'))
+            for dt in svc_datetimes])
     template_service_datetime = """<select name='template-datetime'>
         <option disabled selected value>Select a date/time...</option>
         {0}
         </select>""".format(svc_datetimes_string)
+
+    # Submit a record to the db
+    if bottle.request.params:
+        # Set current serviceID
+        global template_service_id
+        current_service = None
+        dt_param = datetime.strptime(bottle.request.params['template-datetime'], '%m/%d/%y %I:%M %p')
+        for dt_i, dt in enumerate(svc_datetimes):
+            if dt == dt_param:
+                svc_ids = [pair[0] for pair in svc_datetimes_with_id]
+                current_service = svc_ids[dt_i]
+        if current_service:
+            template_service_id = current_service
+        create_record(bottle.request.params)
 
     cursor.execute("""
     SELECT Person.First_Name, Person.Last_Name
     FROM Service JOIN Person ON Service.Songleader_ID = Person.Person_ID
     """)
     songleaders = cursor.fetchall()
-    songleaders_string = '\n'.join(['<option>{}</option>'.format(name[0]) for name in songleaders])
+    songleaders_string = '\n'.join(
+        ['<option>{}</option>'.format(name[0])
+            for name in songleaders])
     songleader = """<select name='songleader'>
         <option disabled selected value>Select a songleader...</option>
         {0}
@@ -78,6 +96,7 @@ def hello():
         </form>
         </body></html>""".format(
             template_service_datetime, songleader, css)
+
 
 # Launch the BottlePy dev server
 if __name__ == "__main__":
